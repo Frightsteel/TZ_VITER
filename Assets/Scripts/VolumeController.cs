8cf83b4e7ab35888a15e961706784f8cfba385bd
Frightsteel <kbelman@yandex.ru>
2022-10-08 18:35:44 +0300

    Main Menu - Update #1
    1) Added start button
    2) Added music volume button. It opens sound settings menu
    3) Added sound settings menu. Now player can change value of the main theme

---
+++ Assets/Scripts/VolumeController.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using TMPro;
+
+public class VolumeController : MonoBehaviour
+{
+    [SerializeField] private Slider _volumeSlider;
+    [SerializeField] private TextMeshProUGUI _volumeValueText;
+
+    [SerializeField] private AudioMixer _mixer;
+
+    private void Start()
+    {
+        LoadVolume();
+    }
+
+    public void UpdateVolumeText(float volume)
+    {
+        _volumeValueText.text = volume.ToString("0.0");
+        _mixer.SetFloat("MusicVol", Mathf.Log10(volume) * 20);
+    }
+
+    public void SaveVolume()
+    {
+        float volumeValue = _volumeSlider.value;
+        PlayerPrefs.SetFloat("VolumeValue", volumeValue);
+        LoadVolume();
+    }
+
+    public void LoadVolume()
+    {
+        float volumeValue = PlayerPrefs.GetFloat("VolumeValue");
+        _volumeSlider.value = volumeValue;
+        AudioListener.volume = volumeValue;
+    }
+}
