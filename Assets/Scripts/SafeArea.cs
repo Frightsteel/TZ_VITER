1e62a3d4f7189ae3402c045a8e14823530c220f5
Frightsteel <kbelman@yandex.ru>
2022-10-11 21:29:24 +0300

    UI - Update #1
    1) Now UI elements always stay in the safe area of the smartphone

--- Assets/Scripts/SafeArea.cs
+++ Assets/Scripts/SafeArea.cs
@@ -1,18 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SafeArea : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private RectTransform rectTransform;
+    private Rect safeArea;
+    private Vector2 minAnchor;
+    private Vector2 maxAnchor;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        rectTransform = GetComponent<RectTransform>();
+
+        safeArea = Screen.safeArea;
+        minAnchor = safeArea.position;
+        maxAnchor = minAnchor + safeArea.size;
+
+        minAnchor.x /= Screen.width;
+        minAnchor.y /= Screen.height;
+        maxAnchor.x /= Screen.width;
+        maxAnchor.y /= Screen.height;
+
+        rectTransform.anchorMin = minAnchor;
+        rectTransform.anchorMax = maxAnchor;
     }
 }
